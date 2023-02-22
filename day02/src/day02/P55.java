7ae20687c96e3219a0c6b3935628bf3e0fd02d21
User <geniejin96@gmail.com>
2023-02-22 17:55:07 +0900

    20230223

---
+++ day02/src/day02/P55.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class P55 {
+
+	public static void main(String[] args) {
+		int a = 2000000000;
+		int b = 2000000000;
+		long A = a;
+		long B = b;
+		long result = A + B;
+		System.out.println(result);
+	}
+
+}
