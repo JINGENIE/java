7e06a224eb8f7f819a4f614804d557c320abf4c5
이진 <@192.168.1.103>
2023-02-26 14:15:04 +0900

    20230226 new laptop //test

---
+++ test1/src/test1/test01.java
@@ -0,0 +1,13 @@
+package test1;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			int i = 5;
+			i ++ ;
+			System.out.println(i);
+	}
+
+}
