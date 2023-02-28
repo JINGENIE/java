b193db20bfa003d6460908f60280aec56ca6b375
geniejin96@gmail.com <geniejin96@gmail.com>
2023-02-28 09:37:57 +0900

    day03

--- day02/src/variable/P73.java
+++ day02/src/variable/P73.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class P73 {
 
 	public static void main(String[] args) {
-		String str =";
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("명령어를 입력하시오(q,i,d,s)");
 		String cmd = sc.next(); //입력된 값을 프로그램안으로 가져오겠다는 뜻
 		System.out.printf("$s를 입력했습니다.", cmd);
-		sc.close(); // close를 해줘야함..
+		sc.close(); // close를 해줘야함..d
 	}
 
 }
